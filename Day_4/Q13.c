a4d1ba0f168d9fe4adb8e79d3a6b601a8f3ca023
Shubham Bahuguna <sbahugunacode@gmail.com>
2026-06-04 10:12:16 +0530

    Day 4 Submitted

--- Day_4/Q13.c
+++ Day_4/Q13.c
@@ -6,6 +6,13 @@ int main(){
     int n, a=0, b=1, c;
     printf("Enter the number of terms :");
     scanf("%d",&n);
+    if(n==1){
+        printf("Fibonacci Series : %d",a);
+    }
+    else if(n==2){
+        printf("Fibonacci Series : %d %d",a,b);
+    }
+    else{
     printf("Fibonacci Series : ");
     for(int i=0; i<n; i++){
         printf("%d ",a);
@@ -13,4 +20,5 @@ int main(){
         a = b;
         b = c;
     }
-}
+            }
+        }
