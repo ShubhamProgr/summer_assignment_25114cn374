557a9d36d01c046fbd4477c71f8bc3a213127da2
Shubham Bahuguna <sbahugunacode@gmail.com>
2026-06-08 10:15:35 +0530

    Day 8

---
+++ Day_8/Q31.c
@@ -0,0 +1,14 @@
+// Program to print a Character Triangle
+
+#include <stdio.h>
+
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            printf("%c",j+64);
+        }
+        printf("\n");
+    }
+    return 0;
+}
