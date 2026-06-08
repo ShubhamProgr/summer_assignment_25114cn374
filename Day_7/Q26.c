338b29c9691812787c5398a599d24fb58c9376cd
Shubham Bahuguna <sbahugunacode@gmail.com>
2026-06-08 09:44:11 +0530

    Day 7

--- Day_7/Q26.c
+++ Day_7/Q26.c
@@ -1,3 +1,30 @@
 // Program of Recursive Fibonacci
 
 #include <stdio.h>
+
+int fibonacci(int n) {
+    if (n==0) {
+        return 0;
+    }
+    else if (n==1) {
+        return 1;
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main() {
+    int terms;
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+    while(terms<0) {
+        printf("Please enter a positive number: ");
+        scanf("%d",&terms);
+    }
+    printf("Fibonacci Series: ");
+    for (int i=0;i<terms;i++) {
+        printf("%d ",fibonacci(i));
+    }
+    return 0;
+}
